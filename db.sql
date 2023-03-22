2369418046093fafd9bd05e504896e8187240bcf
unknown <sanmartinandres48@gmail.com>
2023-03-22 15:33:39 -0300

    Creación de tablas libros y bibliotecarios

--- db.sql
+++ db.sql
@@ -3,8 +3,24 @@ USE test
 
 CREATE TABLE libros(
     id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    title TEXT NOT NULL,
+    titulo TEXT NOT NULL,
     descripcion TEXT NOT NULL,
     autor TEXT NOT NULL,
     paginas INT(5) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS bibliotecarios(
+    id INT(12) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    cedula INT(12) NOT NULL,
+    nombre TEXT NOT NULL,
+    fecha_insercion DATE NOT NULL DEFAULT NOW(),
+    email TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS prestamos(
+    id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    fecha_prestamo DATE NOT NULL DEFAULT NOW(),
+    fecha_devolucion DATE NOT NULL DEFAULT NOW(),
+    libros TEXT NOT NULL,
+    bibliotecario TEXT NOT NULL
+)
